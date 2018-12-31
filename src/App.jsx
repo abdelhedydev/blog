82fa05c57f940bbb8e58f6e38c1f33e9637b8f94
unknown <abdelhedi.hlel@oyez.fr>
2018-12-31 16:26:17 +0100

    app refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+
 import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
@@ -10,30 +9,16 @@ import Posts from './components/Posts';
 import Post from './components/Post';
 import NewPost from './components/NewPost';
 
-const defaultState = {
-  isEditMode: false,
-};
-
-const client = new ApolloClient({
-  uri: 'https://api-euwest.graphcms.com/v1/cjq25vrke5pd701drjsgxbjro/master',
-  clientState: {
-    defaults: defaultState,
-    resolvers: {},
-  },
-});
-
 const App = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route component={Posts} exact path="/" />
-          <Route component={NewPost} exact path="/post/new" />
-          <Route component={Post} path="/post/:id" />
-        </Switch>
-      </div>
-    </Router>
-  </ApolloProvider>
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route component={Posts} exact path="/" />
+        <Route component={NewPost} exact path="/post/new" />
+        <Route component={Post} path="/post/:id" />
+      </Switch>
+    </div>
+  </Router>
 );
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+  isEditMode: false,
+};
+
+const client = new ApolloClient({
+  uri: 'https://api-euwest.graphcms.com/v1/cjq25vrke5pd701drjsgxbjro/master',
+  clientState: {
+    defaults: defaultState,
+    resolvers: {},
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
